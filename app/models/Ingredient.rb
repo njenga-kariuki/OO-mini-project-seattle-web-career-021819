4e30fd36d653c9f3fdbb7c82eb7da5be4db38784
njenga <njengak@me.com>
2019-02-25 11:35:44 -0800

    mini project w/ @bbpak

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,23 @@
+class Ingredient
+  attr_accessor :name
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.most_common_allergen
+    counter_hash = {}
+    Allergen.all.each do |allergen|
+      counter_hash[allergen.ingredient] ||= 0
+      counter_hash[allergen.ingredient] += 1
+    end
+
+    counter_hash.key(counter_hash.values.max)
+  end
+
+  def self.all
+    @@all
+  end
+end
---
+++ app/models/RecipeCard.rb
@@ -0,0 +1,18 @@
+class RecipeCard
+  attr_accessor :user, :recipe, :date, :rating
+
+  @@all = []
+
+  def initialize(user,recipe,rating,date = DateTime.now)
+    @user = user
+    @recipe = recipe
+    @date = date
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
